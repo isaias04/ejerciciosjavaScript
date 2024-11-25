7fc8386bbf6e3797296320d699491f685447bbd9
Isa04 <isaias04>
2024-11-25 12:17:46 -0600

    first commit

---
+++ archi.js
@@ -0,0 +1,13 @@
+var nombre = 'Isaias';
+let apellido = 'Segovia';
+var numero_1 = 20;
+let numero_2 = 20;
+var suma = numero_1 + numero_2;
+var Resta = numero_1 - numero_2;
+const Multiplicar = numero_1 * numero_2
+
+document.write(nombre +' '+ apellido);
+document.write('<br>')
+document.write('los resultados de suma y resta son igual ' + Resta, ' ', + suma );
+document.write('<br>')
+document.write('los resultados de la multiplicacion es igual ' + Multiplicar );
---
+++ cap.js
@@ -0,0 +1,5 @@
+let nombre, apellido;
+nombre = prompt('ingrese su nombre');
+apellido = prompt('ingrese su apellido');
+
+document.write('su nombre es '+nombre +' '+ apellido);
